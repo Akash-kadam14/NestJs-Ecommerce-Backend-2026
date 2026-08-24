98d6994cea31fbcdfb2246ed87d946951abd4e70
akash <akashgit14@gmail.com>
2026-08-24 19:11:06 +0530

    add jwt authentication startegy using passport-jwt and create jwtGuard for authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/createUserDTO';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post('createUser')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getUserProfile(@Request() req) {
+    return this.usersService.getUserProfile(req.user.sub);
   }
 }
